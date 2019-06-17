d13c6599b64420f6c361e6a5d5eab6d027a2397b
Semenov Evgeny <semenovem@gmail.com>
2019-06-17 20:05:19 +0300

    create dir

---
+++ src/mod/PhotosViewer/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+import css from './style.module.css';
+
+type Props = {}
+
+class PhotosViewer extends React.Component<Props> {
+
+  public render() {
+    return (
+      <div className={css.photosViewer}>
+
+        asdasdf
+      </div>
+    );
+  }
+}
+
+
+
+export default PhotosViewer;
