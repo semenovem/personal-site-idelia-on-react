4205eb9fa42b3ffcd71e1ec2117ce826df25bbd7
EVGENY SEMENOV <semenovem@gmail.coml>
2020-01-19 17:29:07 +0300

    site movie - reduces the size of awards icons

--- src/sites/movies/SeeMyVoice/blocks/Awards/index.tsx
+++ src/sites/movies/SeeMyVoice/blocks/Awards/index.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import cn from 'classnames';
-
-import { WinSize, WinSizeCtx } from 'ctx/WinSize';
 
 import award001 from './assets/award001.png';
 import award002 from './assets/award002.png';
@@ -13,50 +10,18 @@ import award006 from './assets/award006.png';
 import css from './css.module.css';
 
 export default function Awards() {
-  const winSize = React.useContext(WinSizeCtx);
-
-  const isMobile = winSize < WinSize.MD;
-
   return (
     <div className={css.awards}>
       <h3 className={css.title}>Awards</h3>
 
-      {isMobile && (
-        <div className={css.wrap}>
-          <div className={css.row}>
-            <img src={award001} className={css.item} alt="" />
-            <img src={award003} className={css.item} alt="" />
-          </div>
-
-          <div className={cn(css.indentTop, css.row)}>
-            <img src={award005} className={css.item} alt="" />
-            <img src={award004} className={css.item} alt="" />
-          </div>
-
-          <div className={cn(css.indentTop, css.row)}>
-            <img src={award006} className={css.item} alt="" />
-            <img src={award002} className={css.item} alt="" />
-          </div>
-
-          {/*<img src={award002} className={cn(css.item, css.indentTop)} alt="" />*/}
-        </div>
-      )}
-
-      {!isMobile && (
-        <div className={cn(css.wrap, css.row)}>
-          <div className={css.row}>
-            <img src={award001} className={css.item} alt="" />
-            <img src={award002} className={css.item} alt="" />
-            <img src={award003} className={css.item} alt="" />
-          </div>
-
-          <div className={cn(css.row, css.indentTop)}>
-            <img src={award005} className={css.item} alt="" />
-            <img src={award004} className={css.item} alt="" />
-            <img src={award006} className={css.item} alt="" />
-          </div>
-        </div>
-      )}
+      <div className={css.wrap}>{renderAwards()}</div>
     </div>
   );
+}
+
+const imgAwards = [award001, award002, award003, award004, award005, award006];
+
+function renderAwards() {
+  // eslint-disable-next-line react/no-array-index-key
+  return imgAwards.map((it, i) => <img src={it} className={css.item} alt="" key={i} />);
 }
