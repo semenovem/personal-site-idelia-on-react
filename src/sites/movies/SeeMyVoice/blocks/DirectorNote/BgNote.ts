a6be8325dd895bc529b70e5869e7ae5629d35fd4
EVGENY SEMENOV <semenovem@gmail.coml>
2020-02-11 08:44:17 +0300

    change background site movie

--- src/sites/movies/SeeMyVoice/blocks/DirectorNote/BgNote.ts
+++ src/sites/movies/SeeMyVoice/blocks/DirectorNote/BgNote.ts
@@ -2,8 +2,8 @@ import { WinSize } from 'ctx/WinSize';
 import { CountdownLevel } from 'ctx/Countdown';
 import { backgroundApplyParams } from 'cnt/ProgressiveImg';
 
-import xs from './assets/bg/bg0--xs.jpg';
-import sm from './assets/bg/bg0--sm.jpg';
+import xs from './assets/bg/bg2--xs.jpg';
+import sm from './assets/bg/bg2--sm.jpg';
 import md from './assets/bg/bg0--md.jpg';
 
 export const params = {
