d261322c6a08a7ca1b4b33f989a5635baf76052a
semenov <semenovem@gmail.com>
2021-08-11 11:26:55 +0300

    links to bomb songs and I'm yours Im

--- src/mod/Music/songs.ts
+++ src/mod/Music/songs.ts
@@ -58,8 +58,7 @@ export const songs: Song[] = [
 
   {
     id: 'bomb',
-    spotify: 'https://open.spotify.com/album/1Wteh2Cc2Lytd30aa5ZU08',
-    // itunes: 'https://music.apple.com/us/album/bomb-single/1472177996',
+    spotify: 'https://open.spotify.com/album/6J8Ombml0AS00p0CP8CvBW',
     itunes: 'https://music.apple.com/us/album/bomb-single/1579621720',
     coverUrl: bomb,
     url: '/songs/bomb.mp3',
@@ -68,9 +67,8 @@ export const songs: Song[] = [
 
   {
     id: 'i_am_yours',
-    spotify: 'https://open.spotify.com/album/3piQPdOwEFGQjv9L2ixC9j',
+    spotify: 'https://open.spotify.com/album/48J1rLEtG92pVPgWwvoMcZ',
     itunes: 'https://music.apple.com/us/album/im-yours-i-m-single/1579549542',
-    // itunes: 'https://music.apple.com/us/album/im-yours-i-m/1471635967?i=1471635973',
     coverUrl: iAmYours,
     url: '/songs/i_am_yours.mp3',
     name: 'I am yours',
