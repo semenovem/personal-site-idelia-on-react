c7b63b7e7ccf07969737db1bcd1fa53182dbcb4a
semenov <semenovem@gmail.com>
2021-11-12 17:07:03 +0400

    add link for youtube ru

--- src/sites/movies/SeeMyVoice/blocks/Header/index.tsx
+++ src/sites/movies/SeeMyVoice/blocks/Header/index.tsx
@@ -5,6 +5,7 @@ import cn from 'classnames';
 // import award2 from 'sites/movies/SeeMyVoice/assets/awards/kino_duel/sm.png';
 // import award0 from 'sites/movies/SeeMyVoice/assets/awards/mfmk_kazan_2019/sm.png';
 import amazonIcon from 'assets/icons/social/amazon.png';
+import youtubeIcon from 'assets/icons/social/youtube.svg';
 
 import Bg from './Bg';
 import Awards from './Awards';
@@ -15,7 +16,8 @@ interface Props {
   className?: string;
 }
 
-const urlAmazon = 'https://www.amazon.com/See-My-Voice-Idelia-Mars/dp/B08C6F92PC/';
+const urlAmazon = 'https://www.amazon.com/See-My-Voice-Idelia-Mars/dp/B08C6F92PC';
+const urlYoutubeRu = 'https://www.youtube.com/watch?v=EWQIMaiwDI4';
 
 export default function Header({ className }: Props) {
   const handleViewTrailer = React.useCallback(() => {
@@ -35,6 +37,9 @@ export default function Header({ className }: Props) {
         <span className={css.amazonAvailable}>NOW AVAILABLE</span>
         <a href={urlAmazon} target="_blank" rel="noopener noreferrer">
           <img className={css.amazonIcon} src={amazonIcon} alt="icon" />
+        </a>
+        <a href={urlYoutubeRu} target="_blank" rel="noopener noreferrer">
+          <img className={css.amazonIcon} src={youtubeIcon} alt="icon" />
         </a>
       </div>
 
